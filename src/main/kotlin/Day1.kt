46490d261859bdd42515eb3dd68104cf8cfe64e1
zstojkovic <00zeljkostojkovic@gmail.com>
2024-12-01 14:24:54 +0100

    day 1 - part two, completed

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -4,6 +4,45 @@ import java.io.File
 import kotlin.math.abs
 
 
+fun main(args: Array<String>) {
+
+    partOne()
+    partTwo()
+}
+
+/*
+       Part two:
+
+       This time, you'll need to figure out exactly how often each number from the left list
+       appears in the right list. Calculate a total similarity score by adding up each number in the left list
+       after multiplying it by the number fo times that number appears in the right list.
+ */
+
+fun partTwo() {
+    var similarityScore = 0;
+    var leftNumbers = mutableListOf<Int>()
+    var rightNumbers = mutableListOf<Int>()
+
+    File("src/main/resources/day1_p1_input.txt").forEachLine { line ->
+        val parts = line.split("   ")
+        leftNumbers.add(parts[0].toInt())
+        rightNumbers.add(parts[1].toInt())
+    }
+
+    for (i in leftNumbers) {
+        var times = 0
+        for (j in rightNumbers) {
+            if (i == j) {
+                times++
+            }
+        }
+        similarityScore += (i * times)
+    }
+
+    println(similarityScore)
+}
+
+
 /*
     Part one:
 
@@ -13,14 +52,7 @@ import kotlin.math.abs
     Within each pair, figure out how far apart the two numbers are; you'll need to add up all of those distances.
     For example:  if you pair up a 3 from the left list with a 7 from right list, the distance apart is 4;
     if you pair up a 9 with a 3, the distance apart is 6.
-
  */
-
-fun main(args: Array<String>) {
-
-    partOne()
-}
-
 private fun partOne() {
     var distance = 0;
     var leftNumbers = mutableListOf<Int>()
