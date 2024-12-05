e763f0051508f875c1310a72662e8f78a6b8bf41
zstojkovic <00zeljkostojkovic@gmail.com>
2024-12-06 00:59:35 +0100

    day 4 - part two, completed

--- src/main/kotlin/Day4.kt
+++ src/main/kotlin/Day4.kt
@@ -8,7 +8,27 @@ fun main() {
     val input = File("src/main/resources/day4_input.txt").readLines()
     val times = partOne(input)
     println(times)
+    val times2 = partTwo(input)
+    println(times2)
+
 }
+
+
+/*
+As the search for the Chief continues, a small Elf who lives on the station tugs on your shirt;
+she'd like to know if you could help her with her word search (your puzzle input). She only has to find one word: XMAS.
+
+This word search allows words to be horizontal, vertical, diagonal, written backwards, or even overlapping other words.
+It's a little unusual, though, as you don't merely need to find one instance of XMAS -
+you need to find all of them. Here are a few ways XMAS might appear, where irrelevant characters have been replaced with .:
+
+..X...
+.SAMX.
+.A..A.
+XMAS.S
+.X....
+
+ */
 
 private fun partOne(input: List<String>): Int {
     var times = 0
@@ -91,7 +111,7 @@ private fun partOne(input: List<String>): Int {
                             input[i - 3][j + 3] == 'S'
 
                         )
-                        times++
+                            times++
                     }
                 }
             }
@@ -100,3 +120,45 @@ private fun partOne(input: List<String>): Int {
     }
     return times
 }
+
+/*
+Looking for the instructions, you flip over the word search to find that this isn't actually an XMAS puzzle;
+it's an X-MAS puzzle in which you're supposed to find two MAS in the shape of an X. One way to achieve that is like this:
+
+        M.S
+        .A.
+        M.S
+
+Irrelevant characters have again been replaced with .
+in the above diagram. Within the X, each MAS can be written forwards or backwards.
+*/
+private fun partTwo(input: List<String>): Int {
+    var times = 0
+    for (i in input.indices) {
+        for (j in input[i].indices) {
+
+            if (input[i][j] == 'A') {
+                if (i >= 1 && i + 1 < input.size &&
+                    j >= 1 && j + 1 < input[i].length
+                ) {
+
+                    val tl = input[i - 1][j - 1]
+                    val tr = input[i - 1][j + 1]
+                    val bl = input[i + 1][j - 1]
+                    val br = input[i + 1][j + 1]
+
+                    val leftDiagonal = listOf(tl, br)
+                    val rightDiagonal = listOf(tr, bl)
+
+                    val hasExpectedLettersLeftDiagonal = leftDiagonal.count { it == 'M' } == 1 && leftDiagonal.count { it == 'S' } == 1
+                    val hasExpectedLettersRightDiagonal = rightDiagonal.count { it == 'M' } == 1 && rightDiagonal.count { it == 'S' } == 1
+
+                    if (hasExpectedLettersLeftDiagonal && hasExpectedLettersRightDiagonal) times++
+
+                }
+            }
+        }
+    }
+    return times
+}
+
